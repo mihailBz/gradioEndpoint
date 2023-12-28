a5c4196265fec47902ea19e16d0d07172ec6a36f
Mykhailo Briazkalo <mbriazka@uwaterloo.ca>
2023-12-28 00:44:59 -0500

    request url updated

--- request_gradio.py
+++ request_gradio.py
@@ -1,17 +1,19 @@
+import random
+
 from gradio_client import Client
 
 
 def request_gradio(
         prompt,
-        seed=0,
+        seed=random.randint(0, 2147483647),
         guidance_scale=15,
         inference_steps=64,
-        url="https://hysts-shap-e.hf.space/--replicas/fwz55/",
+        url="https://hysts-shap-e.hf.space/",
         api_name="/text-to-3d"
 ):
-    return Client(url).predict(
+    return Client(url, verbose=False).predict(
         prompt,  # str  in 'Prompt' Textbox component
-        seed,  # float (numeric value between 0 and 2147483647) in 'Seed' Slider component
+        random.randint(0, 2147483647),  # float (numeric value between 0 and 2147483647) in 'Seed' Slider component
         guidance_scale,  # float (numeric value between 1 and 20) in 'Guidance scale' Slider component
         inference_steps,  # float (numeric value between 2 and 100) in 'Number of inference steps' Slider component
         api_name=api_name
